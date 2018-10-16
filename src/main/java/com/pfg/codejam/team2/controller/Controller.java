797a166c15b1acba7073443cc3f258b164a81cd7
Rohret, Lucas <Rohret.Lucas@principal.com>
2018-10-16 11:14:56 -0500

    Initial commit

---
+++ src/main/java/com/pfg/codejam/team2/controller/Controller.java
@@ -0,0 +1,24 @@
+package com.pfg.codejam.team2.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class Controller {
+
+	private static final Logger LOG = Logger.getLogger(Controller.class);
+
+	@RequestMapping(value="/request", method = RequestMethod.POST)
+	public ResponseEntity<String> getData(@RequestParam("id") long id) {
+
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+}
